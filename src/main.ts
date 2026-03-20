69d2a8b1f87f98223c397a0ec39669a045f9afe3
ytower <ytower@proton.me>
2026-03-20 13:34:34 -0300

    fix: skip old quiz

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import { StrictMode, createElement } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 
+if (!window.location.hash.toLowerCase().includes('dungeon')) {
+	window.location.hash = '#dungeon';
+}
+
 const root = document.getElementById('app');
 
 if (!root) {
